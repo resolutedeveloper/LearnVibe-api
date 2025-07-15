4ba86c11d13243c473a2053ee5c93fa20bc4f707
Roshandudhat <roshandudhat10@gmail.com>
2025-07-15 15:14:56 +0530

    add models

--- src/middlewares/multerConfig.ts
+++ src/middlewares/multerConfig.ts
@@ -1,28 +1,22 @@
 import multer from 'multer';
 import path from 'path';
 
-// const storage = multer.diskStorage({
-//   destination: (_req, _file, cb) => {
-//     cb(null, 'uploads/');
-//   },
-//   filename: (_req, file, cb) => {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-//     //const uniqueSuffix = '123';
-
-//      const ext = path.extname(file.originalname);
-//      //const ext = '123';
-
-//     cb(null, file.fieldname + '-' + uniqueSuffix + ext);
-//   },
-// });
-
 const storage = multer.diskStorage({
   destination: (_req, _file, cb) => {
     cb(null, 'uploads/');
   },
   filename: (_req, file, cb) => {
-    cb(null, file.originalname); // ✅ Keep the same name
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+     const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + uniqueSuffix + ext);
   },
 });
-
+// const storage = multer.diskStorage({
+//   destination: (_req, _file, cb) => {
+//     cb(null, 'uploads/');
+//   },
+//   filename: (_req, file, cb) => {
+//     cb(null, file.originalname); // ✅ Keep the same name
+//   },
+// });
 export const upload = multer({ storage: storage });
