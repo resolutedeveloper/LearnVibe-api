5cea3390f9971b9d0d2ae81d1031de29e7a5f5e4
Roshandudhat <roshandudhat10@gmail.com>
2025-07-15 14:49:44 +0530

    add models

--- src/middlewares/multerConfig.ts
+++ src/middlewares/multerConfig.ts
@@ -1,14 +1,27 @@
 import multer from 'multer';
 import path from 'path';
 
+// const storage = multer.diskStorage({
+//   destination: (_req, _file, cb) => {
+//     cb(null, 'uploads/');
+//   },
+//   filename: (_req, file, cb) => {
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+//     //const uniqueSuffix = '123';
+
+//      const ext = path.extname(file.originalname);
+//      //const ext = '123';
+
+//     cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+//   },
+// });
+
 const storage = multer.diskStorage({
   destination: (_req, _file, cb) => {
     cb(null, 'uploads/');
   },
   filename: (_req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    const ext = path.extname(file.originalname);
-    cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+    cb(null, file.originalname); // ✅ Keep the same name
   },
 });
 
