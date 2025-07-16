20ec44c766b8b7a49a9b8ecfbba350c810f9b57a
SohamRS9325 <soham@resolutesolutions.in>
2025-07-16 17:36:54 +0530

    addded cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import userRoutes from './routes/index.routes';
 import { connectDB } from './db/database';
 import { DecryptFE, EncryptFE, EncryptBE, DecryptBE } from './utils/encrypt';
@@ -7,6 +8,7 @@ import { DecryptFE, EncryptFE, EncryptBE, DecryptBE } from './utils/encrypt';
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1', userRoutes);
