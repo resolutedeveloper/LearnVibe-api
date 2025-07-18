9efc49b6d868dfc03c684ddb1912babddd2de44c
Roshandudhat <roshandudhat10@gmail.com>
2025-07-18 10:13:40 +0530

    add models

--- src/routes/adminSubscription.routes.ts
+++ src/routes/adminSubscription.routes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import { subscription } from '../controllers/subscriptionplan.controller';
+import { subscription,subscription_update,getSubscriptionById,getAllSubscriptions, permanentDeleteSubscription} from '../controllers/subscriptionplan.controller';
 import { tokenVerification } from '../middlewares/tokenVerification'; // ✅ match name: tokenVerification
 import { AddSubscriptionValidationSchema } from '../validations/subscriptionPlanValidation';
 import { asyncHandler } from '../utils/asyncHandler';
 import { validateRequest } from '../middlewares/validateRequest';
 const router = express.Router();
 router.post('/', validateRequest(AddSubscriptionValidationSchema),tokenVerification, asyncHandler(subscription));
-
+router.put('/:id', validateRequest(AddSubscriptionValidationSchema),tokenVerification, asyncHandler(subscription_update));
+router.get('/:id', tokenVerification, asyncHandler(getSubscriptionById));
+router.get('/', tokenVerification, asyncHandler(getAllSubscriptions));
+router.delete('/:id', tokenVerification, asyncHandler(permanentDeleteSubscription));
 export default router;
